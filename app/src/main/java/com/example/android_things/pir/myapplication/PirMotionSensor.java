4b30a685e693c4af95507772c1e3c00d6474b355
lizhieffe <lizhieffe@gmail.com>
2017-12-21 00:04:15 -0800

    update comment

--- app/src/main/java/com/example/android_things/pir/myapplication/PirMotionSensor.java
+++ app/src/main/java/com/example/android_things/pir/myapplication/PirMotionSensor.java
@@ -24,8 +24,16 @@ class PirMotionSensor implements MotionSensor {
     @Override
     public void startup() {
         try {
+            // What direction we expect data to travel. This is a sensor so we expect it to send us
+            // data. Therefore we will use Gpio.DIRECTION_IN.
             bus.setDirection(Gpio.DIRECTION_IN);
+            // GPIO is binary (true or false) so we are declaring what voltage signal equates to
+            // true, a low or high voltage. For our sensor according to the datasheet a high voltage
+            // means movement. Therefore we will use Gpio.ACTIVE_HIGH.
             bus.setActiveType(Gpio.ACTIVE_HIGH);
+            // This is what state change we want monitor, from high voltage to low, low to high or
+            // both. Meaning we could listen for movement starting, ending or both. We want to
+            // listen for movement starting. Therefore we will use Gpio.EDGE_RISING.
             bus.setEdgeTriggerType(Gpio.EDGE_RISING);
         } catch (IOException e) {
             throw new IllegalStateException("Sensor can't start - App is foobar'd", e);
