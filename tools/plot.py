1045038902ec782b36c0dbd706eb2b75335508f1
lizhieffe <lizhieffe@gmail.com>
2018-01-17 01:33:33 -0800

    refactor plot script

--- tools/plot.py
+++ tools/plot.py
@@ -5,13 +5,9 @@ import matplotlib
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+import sys
 from matplotlib import gridspec
 from enum import Enum
-
-# Set timezone which will be used by x-axis.
-matplotlib.rcParams['timezone'] = 'US/Pacific'
-dayFmt=mdates.DateFormatter('%a-%b-%d')
-hrFmt=mdates.DateFormatter('%H:00')
 
 class DataSource(Enum):
     PIR = 1
@@ -19,147 +15,187 @@ class DataSource(Enum):
     ACCEL = 3
     GYRO = 4
 
+def getPlotRow(dataSource):
+    if dataSource == DataSource.PIR:
+        return 1
+    elif dataSource == DataSource.MIC_AMPLITUDE:
+        return 1
+    elif dataSource == DataSource.ACCEL:
+        return 3
+    elif dataSource == DataSource.GYRO:
+        return 3
+    else:
+        assert False, "ERROR: Plot row is not defined"
+
 enabledPlotDataSource = [DataSource.PIR, DataSource.MIC_AMPLITUDE, DataSource.ACCEL, DataSource.GYRO]
 
 def getGridSpec():
     rows = 0
     for pds in enabledPlotDataSource:
-        if pds == DataSource.PIR:
-            rows += 1
-        elif pds == DataSource.MIC_AMPLITUDE:
-            rows += 1
-        elif pds == DataSource.ACCEL:
-            rows += 3
-        elif pds == DataSource.GYRO:
-            rows += 3
+        rows += getPlotRow(pds)
     return rows
 
-currGridRow = 0
+def addPirPlot(dataDir, gridSpec, currGridRow, sharedXAxis):
+    dataFileList = glob.glob(dataDir + '/hc_sr_501_*')
+    data = []  # list of ndarray
+    for filePath in dataFileList:
+      print filePath
+      data.append(np.genfromtxt(filePath, delimiter="\n").flatten())
+    # flatten the list of ndarray to 1D ndarray
+    data = np.concatenate(data).ravel()
+    data = mdates.epoch2num(data/1000)
+    
+    # Plot histogram.
+    ax = plt.subplot(gridSpec[currGridRow], sharex = sharedXAxis)
+    ax.hist(data, bins=1400, color='lightblue')
+    
 
-gs = gridspec.GridSpec(getGridSpec(), 1)
+    return [ax]
 
-dataDir = "/tmp/sensor_data"
-# dataDir = "./sensor_data"
+def addMicAmplitudePlot(dataDir, gridSpec, currGridRow, sharedXAxis):
+    dataFileList = glob.glob(dataDir + '/mic_amplitude_*')
+    amplitude_data = []  # list of ndarray
+    for filePath in dataFileList:
+      print filePath
+      with open(filePath) as f:
+          for line in f:
+              nums = [float(x) for x in line.split(" ")]
+              amplitude_data.append(nums)
+    
+    x = map(lambda x: mdates.epoch2num(int(x[0] / 1000)), amplitude_data)
+    y = map(lambda x: x[1], amplitude_data)
+    ax = plt.subplot(gridSpec[currGridRow], sharex = sharedXAxis)
+    ax.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
 
-dataFileList = glob.glob(dataDir + '/hc_sr_501_*')
-data = []  # list of ndarray
-for filePath in dataFileList:
-  print filePath
-  data.append(np.genfromtxt(filePath, delimiter="\n").flatten())
-# flatten the list of ndarray to 1D ndarray
-data = np.concatenate(data).ravel()
-data = mdates.epoch2num(data/1000)
+    return [ax]
 
-# Plot histogram.
-ax0 = plt.subplot(gs[currGridRow])
-currGridRow += 1
-ax0.hist(data, bins=1400, color='lightblue')
+def addAccelPlot(dataDir, gridSpec, currGridRow, sharedXAxis):
+    dataFileList = glob.glob(dataDir + '/mpu_6500_accel_*')
+    accel_data = []  # list of ndarray
+    for filePath in dataFileList:
+      print filePath
+      with open(filePath) as f:
+          for line in f:
+              nums = [float(x) for x in line.split(" ")]
+              accel_data.append(nums)
+    
+    x = map(lambda x: mdates.epoch2num(int(x[0] / 1000)), accel_data)
+    y = map(lambda x: x[1], accel_data)
+    ax1 = plt.subplot(gridSpec[currGridRow], sharex = sharedXAxis)
+    ax1.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax1.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
+    
+    y = map(lambda x: x[2], accel_data)
+    ax2 = plt.subplot(gridSpec[currGridRow + 1], sharex = ax1)
+    ax2.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax2.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
+    
+    y = map(lambda x: x[3], accel_data)
+    ax3 = plt.subplot(gridSpec[currGridRow + 2], sharex = ax2)
+    ax3.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax3.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
 
-# Set x-axis format.
-ax0.xaxis.set_major_locator(mdates.DayLocator())
-ax0.xaxis.set_major_formatter(dayFmt)
-ax0.xaxis.set_minor_locator(mdates.HourLocator(byhour=[2,4,6,8,10,12,14,16,18,20,22,24]))
-ax0.xaxis.set_minor_formatter(hrFmt)
-
-
-
-dataFileList = glob.glob(dataDir + '/mic_amplitude_*')
-amplitude_data = []  # list of ndarray
-for filePath in dataFileList:
-  print filePath
-  with open(filePath) as f:
-      for line in f:
-          nums = [float(x) for x in line.split(" ")]
-          amplitude_data.append(nums)
-
-x = map(lambda x: mdates.epoch2num(int(x[0] / 1000)), amplitude_data)
-y = map(lambda x: x[1], amplitude_data)
-ax1 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax1.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax1.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
-
-
-
-dataFileList = glob.glob(dataDir + '/mpu_6500_accel_*')
-accel_data = []  # list of ndarray
-for filePath in dataFileList:
-  print filePath
-  with open(filePath) as f:
-      for line in f:
-          nums = [float(x) for x in line.split(" ")]
-          accel_data.append(nums)
-
-x = map(lambda x: mdates.epoch2num(int(x[0] / 1000)), accel_data)
-y = map(lambda x: x[1], accel_data)
-ax1 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax1.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax1.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
-
-y = map(lambda x: x[2], accel_data)
-ax2 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax2.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax2.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
-
-y = map(lambda x: x[3], accel_data)
-ax3 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax3.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax3.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
+    return [ax1, ax2, ax3]
 
 
 
-dataFileList = glob.glob(dataDir + '/mpu_6500_gyro_*')
-gyro_data = []  # list of ndarray
-for filePath in dataFileList:
-  print filePath
-  with open(filePath) as f:
-      for line in f:
-          nums = [float(x) for x in line.split(" ")]
-          gyro_data.append(nums)
+def addGyroPlot(dataDir, gridSpec, currGridRow, sharedXAxis):
+    dataFileList = glob.glob(dataDir + '/mpu_6500_gyro_*')
+    gyro_data = []  # list of ndarray
+    for filePath in dataFileList:
+      print filePath
+      with open(filePath) as f:
+          for line in f:
+              nums = [float(x) for x in line.split(" ")]
+              gyro_data.append(nums)
+    
+    x = map(lambda x: mdates.epoch2num(int(x[0] / 1000)), gyro_data)
+    y = map(lambda x: x[1], gyro_data)
+    ax4 = plt.subplot(gridSpec[currGridRow], sharex = sharedXAxis)
+    ax4.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax4.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
+    
+    y = map(lambda x: x[2], gyro_data)
+    ax5 = plt.subplot(gridSpec[currGridRow + 1], sharex = ax4)
+    ax5.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax5.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
+    
+    y = map(lambda x: x[3], gyro_data)
+    ax6 = plt.subplot(gridSpec[currGridRow + 2], sharex = ax5)
+    ax6.plot(x, y)
+    # remove last tick label for the second subplot
+    yticks = ax6.yaxis.get_major_ticks()
+    yticks[-1].label1.set_visible(False)
 
-x = map(lambda x: mdates.epoch2num(int(x[0] / 1000)), gyro_data)
-y = map(lambda x: x[1], gyro_data)
-ax4 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax4.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax4.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
+    return [ax4, ax5, ax6]
 
-y = map(lambda x: x[2], gyro_data)
-ax5 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax5.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax5.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
+def main(argv):
+    currGridRow = 0
+    gs = gridspec.GridSpec(getGridSpec(), 1)
+    
+    dataDir = "/tmp/sensor_data"
+    # dataDir = "./sensor_data"
 
-y = map(lambda x: x[3], gyro_data)
-ax6 = plt.subplot(gs[currGridRow], sharex = ax0)
-currGridRow += 1
-ax6.plot(x, y)
-# remove last tick label for the second subplot
-yticks = ax6.yaxis.get_major_ticks()
-yticks[-1].label1.set_visible(False)
+    # Set timezone which will be used by x-axis.
+    matplotlib.rcParams['timezone'] = 'US/Pacific'
+    dayFmt=mdates.DateFormatter('%a-%b-%d')
+    hrFmt=mdates.DateFormatter('%H:00')
+    
+    ax0 = plt.axes()
+    
+    for pds in enabledPlotDataSource:
+        plotted = False
+        if pds == DataSource.PIR:
+            localAx = addPirPlot(dataDir, gs, currGridRow, ax0)
+            plotted = True
+        elif pds == DataSource.MIC_AMPLITUDE:
+            localAx = addMicAmplitudePlot(dataDir, gs, currGridRow, ax0)
+            plotted = True
+        elif pds == DataSource.ACCEL:
+            localAx = addAccelPlot(dataDir, gs, currGridRow, ax0)
+            plotted = True
+        elif pds == DataSource.GYRO:
+            localAx = addGyroPlot(dataDir, gs, currGridRow, ax0)
+            plotted = True
+    
+        if plotted == True:
+            if currGridRow == 0:
+                ax0 = localAx[0]
+    
+                # Set x-axis format.
+                ax0.xaxis.set_major_locator(mdates.DayLocator())
+                ax0.xaxis.set_major_formatter(dayFmt)
+                ax0.xaxis.set_minor_locator(mdates.HourLocator(byhour=[2,4,6,8,10,12,14,16,18,20,22,24]))
+                ax0.xaxis.set_minor_formatter(hrFmt)
+    
+            currGridRow += getPlotRow(pds)
+    
+            i = 0
+            for lax in localAx:
+                # remove last tick label for the second subplot
+                yticks = lax.yaxis.get_major_ticks()
+                yticks[-1].label1.set_visible(False)
+    
+                # TODO: remove the redundant x ticks.
+                #plt.setp(lax.get_xticklabels(), visible=False)
+    
+    # remove vertical gap between subplots
+    plt.subplots_adjust(hspace=.0)
+    
+    plt.show()
 
-
-
-
-plt.setp(ax0.get_xticklabels(), visible=False)
-
-
-# remove vertical gap between subplots
-plt.subplots_adjust(hspace=.0)
-
-plt.show()
+if __name__ == "__main__":
+    main(sys.argv)
